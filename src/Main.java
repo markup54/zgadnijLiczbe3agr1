f7d9be9c14725e4f30ae3b022767cf5923f04525
markup54 <marzenakupka@tlen.pl>
2025-09-15 12:40:00 +0200

    typy proste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //tu program główny
+        System.out.println("Dzień dobry");
+
+        //typy proste
+        //pisane z małej litery, zmienne a nie obiekty -> nie mają pól i metod
+        //typy całkowite
+        byte liczba1 =100;
+        short liczba2 =300;
+        int liczba3 = 1234576;
+        long liczba4 =612564527;
+        System.out.println("Twoja liczba "+liczba1);
+
+        //typy zmiennoprzecinkowe, rzeczywiste , z ułamkiem
+        float liczbaRzeczywista1 = 3.14f;//musi być na końcu f
+        double liczbaRzeczywista2 = 23.456789;
+        System.out.println("Liczba rzeczywista "+liczbaRzeczywista2);
+
+        //typ logiczny
+        boolean czyPrawda = true; //może być też false
+        //typ znakowy
+        char znak = 'a';
+
+
+        //
     }
 }
